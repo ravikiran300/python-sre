b1c9b5fd97d8e2c2a125c8284e33ac3db7ecba26
ravikiran300 <ravadaravikiran300@gmail.com>
2022-08-09 10:05:12 +0530

    adding ravi.py

---
+++ ravi.py
@@ -0,0 +1,12 @@
+
+
+print('hello ravikiran')
+myname=input()
+print('good to meet you' ,  myname)
+print(len(myname))
+
+age = input()
+print('my age is ' +  str(int(age) + 1 + ' in a year' ))
+print('You will be ' + str(int(age) + 1  + ' in a year.'))
+
+
