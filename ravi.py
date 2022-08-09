57a25faa89e4eb91712e87c0a9583230203471f2
ravikiran300 <54069290+ravikiran300@users.noreply.github.com>
2022-08-09 10:18:55 +0530

    Update ravi.py

--- ravi.py
+++ ravi.py
@@ -6,7 +6,6 @@ print('good to meet you' ,  myname)
 print(len(myname))
 
 age = input()
-print('my age is ' +  str(int(age) + 1 + ' in a year' ))
 print('You will be ' + str(int(age) + 1  + ' in a year.'))
 
 
